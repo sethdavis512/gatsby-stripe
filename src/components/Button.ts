1a2b9affedccd4ab4124503dfc4e733c865bb6e8
Seth Davis <sethdavis512@gmail.com>
2020-11-28 16:14:42 -0600

    Update packages. Update file extension.

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -4,12 +4,17 @@ const Button = styled.button`
     background-color: ${({ theme }) => theme.primary};
     border-radius: 12px;
     border: 2px solid ${({ theme }) => theme.primary};
-    color: ${({ theme }) => theme.white};
+    color: white;
     font-size: 16px;
     padding: 0.5rem 1rem;
 
     :hover {
         background-color: ${({ theme }) => theme.secondary};
+        border: 2px solid ${({ theme }) => theme.primary};
+    }
+
+    :disabled {
+        background-color: ${({ theme }) => theme.primary};
         border: 2px solid ${({ theme }) => theme.primary};
     }
 `
