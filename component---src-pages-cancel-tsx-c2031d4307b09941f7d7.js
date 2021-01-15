9efb5e8b923838719cd04d79de25014c7f07be33
Seth Davis <sethdavis512@gmail.com>
2021-01-15 16:05:50 -0600

    Updates

---
+++ component---src-pages-cancel-tsx-c2031d4307b09941f7d7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{vLMe:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Wbzz"),c=t("MCUY"),o=t("soUV");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(c.a,null,l.a.createElement("h1",null,"Cancelled."),l.a.createElement("p",null,"You have not been charged."),l.a.createElement(r.Link,{to:"/"},"Return to store")))}}}]);
+//# sourceMappingURL=component---src-pages-cancel-tsx-c2031d4307b09941f7d7.js.map
